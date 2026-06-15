429023388402d6fa9b855ca2ce0ec85eb7b0d4d0
Mauricio Martins Taques <mtaquesfilho@gmail.com>
2026-06-15 00:52:18 -0300

    Delete mainWindow.py

--- mainWindow.py
+++
@@ -1,9 +0,0 @@
-from PyQt6.QtGui import QIcon
-from PyQt6.QtWidgets import QMainWindow
-
-
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("Gerência de Farmácia")
-        self.showMaximized()
